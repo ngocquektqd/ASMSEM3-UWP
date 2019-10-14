b304723f8747abf9bcdcbdc3989b40f1fb5910f0
Your Name <you@example.com>
2019-10-14 09:25:13 +0700

    update code

--- Assignment/Constant/ApiUrl.cs
+++ Assignment/Constant/ApiUrl.cs
@@ -8,8 +8,8 @@ namespace Assignment.Constant
 {
     class ApiUrl
     {
-        //public const string API_BASE_URL = "https://2-dot-backup-server-003.appspot.com/_api/v2";
-        public const string API_BASE_URL = "https://2-dot-backup-server-001.appspot.com/_api/v2";
+        public const string API_BASE_URL = "https://2-dot-backup-server-003.appspot.com/_api/v2";
+        //public const string API_BASE_URL = "https://2-dot-backup-server-001.appspot.com/_api/v2";
         public const string LOGIN_URL = API_BASE_URL + "/members/authentication";
         public const string GET_INFORMATION_URL = API_BASE_URL + "/members/information";
         public const string GET_FREE_SONG_URL = API_BASE_URL + "/songs/get-free-songs";
